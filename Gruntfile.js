a4e382b4b89cf5d0c226423d64925c68ccf2ea11
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-01-22 00:08:14 -0600

    added mocha/chai instead of nodeunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,20 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          clearRequireCache: true
+        },
+        src: ['test/**/*.js']
+      },
+      simple: {
+        options: {
+          reporter: 'spec',
+          clearRequireCache: true
+        }
+      }
     },
     jshint: {
       options: {
@@ -26,23 +38,45 @@ module.exports = function(grunt) {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
       },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
+      js: {
+        options: {
+          spawn: false
+        },
+        files: ["lib/**/*.js", "test/**/*_test.js"],
+        //tasks: ['mochaTest']
+      }
+      //lib: {
+        //files: '<%= jshint.lib.src %>',
+        //tasks: ['jshint:lib', 'nodeunit']
+      //},
+      //test: {
+        //files: '<%= jshint.test.src %>',
+        //tasks: ['jshint:test', 'nodeunit']
+      //},
     },
   });
 
+  // On watch events configure mochaTest to run only on the test if it is one
+  // otherwise, run the whole testsuite
+  var defaultSimpleSrc = grunt.config('mochaTest.simple.src');
+  grunt.event.on('watch', function(action, filepath) {
+    grunt.config('mochaTest.simple.src', defaultSimpleSrc);
+    if (filepath.match('test/')) {
+      grunt.config('mochaTest.simple.src', filepath);
+      grunt.task.run('mochaTest:simple');
+    } else if (filepath.match('lib/')) {
+      var testpath = filepath.replace('lib', 'test').replace('.js', '_test.js');
+      grunt.config('mochaTest.simple.src', testpath);
+      grunt.task.run('mochaTest:simple');
+    }
+  });
+
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
---
+++ test/track_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var trackjs = require('../lib/track.js'),
+    expect = require('chai').expect;
+
+describe('awesome function', function() {
+  it('should return awesome', function() {
+    expect(trackjs.awesome()).to.equal('awesome');
+  });
+});
